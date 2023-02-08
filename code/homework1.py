9cb2ca69fa5a9ce8412b990c29e61e76b4ef8936
Arch-enemy <csiddu98@gmail.com>
2023-02-07 22:13:09 -0600

    horribly wrong dfs scratch and work again

--- code/homework1.py
+++ code/homework1.py
@@ -29,50 +29,53 @@ def successor(cr,cc,matrix,successors,visited):
     #creating the range of movement 
     #if the direction is east the movement should only be lateral increment i.e. column + range(1 to (size of matrix))
     # tsuc = []
-    if (cr,cc) not in visited:
-        visited.append((cr,cc))
-        print(visited)
+    #if (cr,cc) not in visited:
+    #    visited.append((cr,cc))
+       # print(visited)
     #adding path to be explored
-    if (cr,cc) not in path:
-        path.append((cr,cc))
-        print(path)
+    #if (cr,cc) not in path:
+    #    path.append((cr,cc))
+    #    print(path)
 
     if(matrix[cr][cc] == 'F'):
-        sucessor.append(matrix[cr][cc])
-        return successor
+        successors.append([cr,cc])
+        #successors.append(matrix[cr][cc])
+        return successors
 
     if (matrix[cr][cc] == 'N'):
         for tr in range(1,len(matrix)):
           north = cr - tr
           if north in range (0,len(matrix)):
                 #print(matrix[cr - tr][cc])
-                print(cr-tr)
-                successors.append(matrix[north][cc])
-                tsuc.append(matrix[north][cc])
+               # print(cr-tr)
+                #successors.append(matrix[north][cc])
+                successors.append([north,cc])
+                # tsuc.append(matrix[north][cc])
                 #if not len(tsuc) == 0:
                    # path.append(cr,cc)
 
     if (matrix[cr][cc] == 'S'):
-        for tc in range(1,len(matrix)):
+        for tr in range(1,len(matrix)):
             print(cr+tr)
             south = cr + tr
             if south in range (0,len(matrix)):
-                print(matrix[south][cc]) 
-                successors.append(matrix[south][cc])
+                successors.append([south,cc]) 
+                #successors.append(matrix[south][cc])
 
     if (matrix[cr][cc] == 'E'):
         for tc in range(1,len(matrix)):
             east = cc+tc
             if east in range (0,len(matrix)):
-                print(matrix[cr][east]) 
+               # print(matrix[cr][east]) 
                 successors.append([cr,east]) # testing on converting list of location to location
 
     if (matrix[cr][cc] == 'W'):
         for tc in range(1,len(matrix)):
             west = (cc-tc)
             if west in range (0,len(matrix)):
-                print(matrix[cr][west]) 
-                successors.append(matrix[cr][west])
+               # print(matrix[cr][west]) 
+                successors.append([cr,west])
+                #successors.append(matrix[cr][west])
 
     if (matrix[cr][cc] == 'NE'):
         for tr in range(1,len(matrix)):
@@ -81,8 +84,8 @@ def successor(cr,cc,matrix,successors,visited):
                     northeastR = cr - tr
                     northeastC = cc + tr
                     if northeastR in range (0,len(matrix)) and northeastC in range (0,len(matrix)):
-                        print(matrix[northeastR][northEastC]) 
-                        successors.append(matrix[NorthEastR][NorthEastC])
+                        successors.append([northeastR,northEastC]) 
+                        #successors.append(matrix[NorthEastR][NorthEastC])
 
     if (matrix[cr][cc] == 'NW'):
         for tr in range(1,len(matrix)):
@@ -91,8 +94,8 @@ def successor(cr,cc,matrix,successors,visited):
                     northwestR = cr - tr
                     northwestC = cc - tr
                     if northwestR in range (0,len(matrix)) and  northwestC in range (0,len(matrix)):
-                        print(matrix[northwestR][northwestC]) 
-                        successors.append(matrix[northwestR][northwestC])
+                        successors.append([northwestR,northwestC]) 
+                        #successors.append(matrix[northwestR][northwestC])
 
     if (matrix[cr][cc] == 'SE'):
         for tr in range(1,len(matrix)):
@@ -101,8 +104,8 @@ def successor(cr,cc,matrix,successors,visited):
                     southeastR = cr + tr
                     southeastC = cc + tr
                     if southeastR in range (0,len(matrix)) and southeastC in range (0,len(matrix)):
-                    #print(matrix[southeastR][southEastC])
-                        successors.append(matrix[southeastR][southeastC])
+                        successors.append([southeastR,southeastC])
+                        #successors.append(matrix[southeastR][southeastC])
 
 
     if (matrix[cr][cc] == 'SW'):
@@ -112,9 +115,10 @@ def successor(cr,cc,matrix,successors,visited):
                     southwestR = cr + tr
                     southwestC = cc - tr
                     if southwestR in range (0,len(matrix)) and southwestC in range (0,len(matrix)):
-                        print(southwestR,southwestC)
-                        successors.append(matrix[southwestR][southwestC])
-
+                        print(matrix[southwestR][southwestC])
+                        #successors.append(matrix[southwestR][southwestC])
+                        successors.append([southwestR,southwestC])        
+                
 def leafcheck(cr,cc,successors):
     if len(successor) == 0:
         return true
@@ -127,9 +131,41 @@ def convert(maze,successors):
     return(listl)
 
 #creating a depth first function
-def dfs(cr ,cc, maze, successors,path,visited):
-    current = maze[cr][cc]
-    successors = successor(cr,cc,maze,visited)
+def dfs(cr,cc,maze,successors,visited):
+    #adding current marker
+    current = ([cr,cc])
+    #adding current to visited
+    visited.append(current)
+    successors.append(current)
+    if len(successors) != 0:
+        print(successors)
+        child = successors.pop()
+        print(child[0],child[1])
+    #calling successor function to add child nodes to successors 
+        successor(child[0],child[1],maze,successors,visited)
+        print(successors)
+        if len(successors) != 0:
+            child = successors.pop()
+            if child in visited :
+                #if loop and visited do not append but pop the other and perform successor
+                child = successors.pop()
+                dfs(child[0],child[1],maze,successors,visited)
+                #successor(child[0],child[1],maze,successors,visited)
+        print(successors)
+        return successors
+
+    #picking the node that is last in the stack i.e. DFS
+    #checking if successor stack is empty
+
+#    if len(successors) != 0:
+#        child = successors.pop()
+#        if  (maze[child[0]][child[1]]) == 'F':
+#            visited.append(child)
+#            print(visited)
+#            return successors
+#        successor(child[0],child[1],maze,successors,visited)
+#    print(visited)
+#    return successors
 
 # current state  
 # add the current state in visited(location), add current state in path(location)
@@ -150,12 +186,11 @@ path = []
 visited = []
 successors = []
 
+dfs(2,0,maze,successors,visited)
+#successor(0,0,maze,successors,visited)
+#print(successors)
 
-
-successor(0,0,maze,successors,visited)
-print(successors)
-
-templist = convert(maze,successors)
-print(templist)
+#templist = convert(maze,successors)
+#print(templist)
 
 
